4c24f26fe61523695ae0b02232c0a5d0cabd85de
Alphie <shikwambana27@gmail.com>
2022-11-05 02:20:03 +0200

    minor change

--- app/src/app/not-found.component.ts
+++ app/src/app/not-found.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  template: '<h2>Page not found</h2>'
+  template: '<h2 style="padding-left: 15px;">Page not found</h2><br><br><p style="padding-left: 15px;"><button routerLink="/">Click here</button> to go home</p>'
 })
 export class PageNotFoundComponent {
 }
